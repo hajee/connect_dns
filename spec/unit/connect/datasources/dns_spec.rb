aa6d89674e92c9900092c23dcb8b9a321f82c0ad
Bert Hajee <hajee@moretIA.com>
2015-05-01 13:06:46 +0200

    Initial commit

---
+++ spec/unit/connect/datasources/dns_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'connect/datasources/dns'
+
+RSpec.describe Connect::Datasources::Dns do
+
+  describe '#initialize' do
+  end
+
+
+  describe "#lookup" do
+  end
+
+end
+
